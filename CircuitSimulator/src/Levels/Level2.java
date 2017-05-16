6b83156085720b4c7fce75fde79aac9243f5f6b1
guanguangua <guanguangua@gmail.com>
2017-05-15 21:42:59 -0700

    Update Level2.java

--- CircuitSimulator/src/Levels/Level2.java
+++ CircuitSimulator/src/Levels/Level2.java
@@ -12,15 +12,25 @@ public class Level2 extends AbstractLevel{
 	 */
 	public Level2() {
 		// TODO Auto-generated constructor stub
-		super(4);
-		level = 2;
-		size = 4;
+		size = 6;
+    size_r1 = 3;
+    size_r2 = 2;
+		size_s = 1;
+    r1remaining = size_r1;
+		r2remaining = size_r2;
+		sremaining = size_s;
+    r_wanted = 2.5;
+    x.addAll(Arrays.asList(44,44,206,352,352,522)); // remember to change the last one!
+		y.addAll(Arrays.asList(11,123,50,12,124,227));   // same
+		this.setR(size - 1, Double.MAX_VALUE);
 	}
 
 	@Override
 	public double findRes() {
-		// TODO Auto-generated method stub
-		return 0;
+		assert(isFull());
+		double finalRes;
+		finalRes = 1/(1/r.get(0) + 1/r.get(1)) + r.get(2) + 1/(1/r.get(3) + 1/r.get(4));
+		return finalRes;
 	}
 	
 }
